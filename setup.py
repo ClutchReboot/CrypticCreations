306d72112403eeed7bf2e63f8d657a418bb01507
ClutchTech <rollout26@gmail.com>
2022-06-01 11:22:39 -0400

    Setup hotfixes.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # python setup.py sdist
 # twine upload dist/*
 
 setup(name='CrypticCreations',
-      version='0.2.1',
+      version='0.2.3',
       description='Create ciphers or random strings for DnD players to try and read aloud for the group.',
       url='https://github.com/ClutchTech/CrypticCreations',
       keywords='dnd ciphers random CrypticCreations',
       author='Clutch_Reboot',
       author_email='clutchshadow26@gmail.com',
       license='GNU General Public License v2.0',
-      packages=['CrypticCreations'],
+      packages=['CrypticCreations', 'CrypticCreations.ciphers'],
       zip_safe=False,
       long_description=open('README.md', 'rt').read(),
       long_description_content_type='text/markdown'
